002ca4d06965f5c0f40e3d7df04d4231cd6f4288
iamqaasim <iamqaasim@gmail.com>
2023-05-16 21:17:18 +0200

    Task 3 - created a countStudents async function

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,56 @@
+const { readFile } = require("fs");
+
+function countStudents(fileName) {
+  const students = {}; // Object to store students by field
+  const fields = {}; // Object to store the count of students in each field
+  let length = 0; // Variable to store the total number of students
+
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (error, data) => {
+      if (error) {
+        reject(Error("Cannot load the database"));
+      } else {
+        const lines = data.toString().split("\n"); // Split file contents into lines
+
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1; // Increment the total number of students
+
+            const field = lines[i].toString().split(","); // Split each line into fields
+
+            // Storing students by field
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+
+            // Counting students in each field
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+
+        const len = length - 1; // Subtracting 1 to exclude the header line
+        console.log(`Number of students: ${len}`);
+
+        // Displaying the count of students and their names for each field
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== "field") {
+            console.log(
+              `Number of students in ${key}: ${value}. List: ${students[
+                key
+              ].join(", ")}`
+            );
+          }
+        }
+        resolve(data);
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
